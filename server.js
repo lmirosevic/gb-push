f6dd62366d8d018ac1b4fc41e6bf994e297bdf6c
Luka Mirosevic <luka@goonbee.com>
2014-05-18 10:40:47 +0200

    Added useful scripts

---
+++ server.js
@@ -0,0 +1,10 @@
+//
+//  server.js
+//  gb-push
+//
+//  Created by Luka Mirosevic on 17/04/2014.
+//  Copyright (c) 2014 Goonbee. All rights reserved.
+//
+
+// Compatibility layer for AWS OpsWorks
+require('./app');
